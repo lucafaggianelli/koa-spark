a493faaf50061d490fb3537fa068cf4fbba53ffa
Luca Faggianelli <luca.faggianelli@gmail.com>
2021-11-03 20:01:04 +0100

    Implement AppConfig with props decorators

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,41 @@
-import dotenv from 'dotenv'
 import fs from 'fs'
+import path from 'path'
+import { cwd } from 'process'
 
-export default class Config {
+import dotenv from 'dotenv'
+import 'reflect-metadata'
+
+type ConfigFieldsMap = Map<string | symbol, FieldOptions>
+
+interface FieldOptions {
+  default?: any
+  type?: string
+}
+
+const FIELDS_METADATA_KEY = Symbol('fields')
+
+const camelToSnakeCase = (str: string) => str.replace(/[A-Z]/g, letter => `_${letter}`).toUpperCase()
+
+export function Field(options: FieldOptions = {}): PropertyDecorator {
+  return (target, key) => {
+    if (!options.type) {
+      options.type = Reflect.getMetadata("design:type", target, key).name
+    }
+
+    const fields: ConfigFieldsMap = Reflect.getMetadata(FIELDS_METADATA_KEY, target) || new Map<string | symbol, FieldOptions>()
+
+    if (!fields.has(key)) {
+      fields.set(key, options)
+    }
+
+    Reflect.defineMetadata(FIELDS_METADATA_KEY, fields, target)
+  }
+}
+
+/**
+ * Application Configuration for setting environment-specific and secret configs
+ */
+export class AppConfig {
   /**
    * NODE_ENV variable. If it's not set, by default it has `'development'` value
    */
@@ -22,24 +56,82 @@ export default class Config {
   /**
    * Server port number
    */
+  @Field({ default: 3000 })
   port: number
 
+  /**
+   * The URL on which the server is hosted
+   */
+  @Field()
+  origin: string
+
   constructor () {
-    const envConfigFileExists = fs.existsSync(`.env.${this.nodeEnv}`)
-
-    dotenv.config({
-      path: envConfigFileExists ? `.env.${this.nodeEnv}` : '.env'
-    })
-
     /**
-     * Init config
+     * Init NODE_ENV
      */
     this.nodeEnv = process.env.NODE_ENV || 'development'
-
     this.isDevelopment = this.nodeEnv === 'development'
     this.isProduction = this.nodeEnv === 'production'
     this.isTesting = this.nodeEnv === 'test' || this.nodeEnv === 'testing'
 
-    this.port = parseInt(process.env.PORT as string) || 3000
+    this.loadEnvFile()
+
+    this.initConfigFields()
+
+    /**
+     * Init *complex* default values that depends on other configs
+     */
+    this.origin = this.origin ?? `http://localhost:${this.port}`
+  }
+
+  private loadEnvFile () {
+    // Load environment configuration
+    const envSpecificFile = path.join(cwd(), `.env.${this.nodeEnv}`)
+    const envFilePath = fs.existsSync(envSpecificFile)
+      ? envSpecificFile
+      : path.join(cwd(), '.env')
+
+    dotenv.config({
+      path: envFilePath
+    })
+  }
+
+  /**
+   * Initialize all the fields values with the values found in the configuration
+   * or their default values
+   */
+  private initConfigFields () {
+    const fields: ConfigFieldsMap = Reflect.getMetadata(FIELDS_METADATA_KEY, this) || new Map<string | symbol, FieldOptions>()
+
+    fields.forEach((fieldOptions, key) => {
+      const value = this.getConfigValue(
+        camelToSnakeCase(key.toString()),
+        fieldOptions.type
+      )
+
+      this[key] = value ?? (fieldOptions ? fieldOptions.default : undefined)
+    })
+  }
+
+  private getConfigValue (name: string, type: string, defaultValue?: any): any {
+    if (type === 'number') {
+      return this.getConfigAsInt(name, defaultValue)
+    } else {
+      return this.getConfigAsString(name, defaultValue)
+    }
+  }
+
+  private getConfigAsString (name: string, defaultValue?: string) {
+    return process.env[name] || defaultValue
+  }
+
+  private getConfigAsInt (name: string, defaultValue: number = 0) {
+    const value = this.getConfigAsString(name)
+
+    if (value) {
+      return parseInt(value)
+    }
+
+    return defaultValue
   }
 }
